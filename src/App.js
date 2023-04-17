f97eee9a779bf411b123ccdc92881955126da515
Mark <65822259+whippet-code@users.noreply.github.com>
2023-04-17 19:53:14 +0000

    Update

--- src/App.js
+++ src/App.js
@@ -14,22 +14,22 @@ import portfolio from "./images/icons8-developer-50.png";
 // Text info for blurb
 const summary = {
   title: "Summary",
-  text: "A dedicated web developer with experience building for front-end and full stack. Passionate about creative design solutions and skilled in React.js, Javascript, CSS, HTML, Node.js & Express. Determined breaker down of problems with a love for collaboration and a passion for continual learning.",
+  text: "A dedicated web developer with experience building for front-end and full stack. Passionate about creative design solutions and skilled in React.js, Javascript, CSS, HTML, Node.js & Express. Determined breaker down of problems with a love for collaboration and a passion for continual learning. Also rather partial to Cyberpunk, pedaling bikes and breathwork. Has many cameras.",
 };
 
 const tech = {
   title: "Tech Stack",
-  text: "Javascript - React - Redux - CSS/HTML - Node.js - Express",
+  text: "Javascript - React - Redux - CSS/HTML - Node.js - Express - MongoDB",
 };
 
 const experience = {
   title: "Experience",
-  text: "Hyperiondev, Web Development — Developer Bootcamp\nDec 2022 - Mar 2023\nCurrently enrolled, intensive three month jobs based skills bootcamp. Covering all aspects of MERN stack for commercial web development environments and deployment.\n\nHarvard — CS50x Intro to Computer Science\nApr 2022 - Nov 2023 - Online\nWide spectrum intro to C.S. giving a solid grounding into algorithms, data structures, correct coding practices and software solutions. Technologies covered; C, Javascript, Python, SQLite3, Flask.\n\nSKILLS\nHighly motivated self-starter.Managing teams under pressure.Coaching & training.Problem solving.Creative design.\nTools - Adobe suite, Google, github",
+  text: "Hyperiondev, Web Development — Developer Bootcamp\nDec 2022 - Mar 2023\nCurrently graduated, intensive three month jobs based skills bootcamp. Covering all aspects of MERN stack for commercial web development environments and deployment.\n\nHarvard — CS50x Intro to Computer Science\nApr 2022 - Nov 2023 - Online\nWide spectrum intro to C.S. giving a solid grounding into algorithms, data structures, correct coding practices and software solutions. Technologies covered; C, Javascript, Python, SQLite3, Flask.\n\nSKILLS\nHighly motivated self-starter. Managing teams and self under pressure. Leading coaching & training workshops. Problem dissection. Creative design.\nTools - Adobe suite, github, Open-ai, VS Code",
 };
 
 const projects = {
   title: "Projects",
-  text: "Photonotes Field log app.\nBuilt a complete web based messaging board app with multi-user log-in and database referencing for Harvard CS50x final project.\nHyperiondev capstone projects.\nJoint founder of PhotoMooch, art therapy / wellbeing collective.",
+  text: "PhotoMooch fullstack multi-user & admin web app, providing creative prompts for users.\nPhotonotes Field log app.\nBuilt a complete web based messaging board app with multi-user log-in and database referencing for Harvard CS50x final project.\nHyperiondev capstone projects.\nJoint founder of PhotoMooch, art therapy / wellbeing collective.",
 };
 
 const cards = [summary, tech, experience, projects];
@@ -47,7 +47,7 @@ const links = [
 
 function App() {
   // const cardComponenets = card.map(item =>
-  //   <Card ingo={item} />)
+  //   <Card info={item} />)
   return (
     <div className="App">
       <div className="left-side">
